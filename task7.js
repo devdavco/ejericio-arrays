364d8397059216d1d6b9337b6a047629556ee603
devdavco <github.ferocity308@passmail.com>
2026-09-24 17:38:00 -0500

    exercise 7, reverse array, pop,slice

--- task7.js
+++ task7.js
@@ -5,7 +5,29 @@ const arr = [
   ]
   
   // ¡Escribe tu código debajo de esta línea!
-  
-  
+
+  arr[2].push(30)// Agrego al final
+  arr[2].unshift(20) //agrego primero
+  arr[2].splice(0,0,20) //agrego primero usando, igual que unshift
+
+console.log(arr[2])  
+
+console.log("Inserto nueva fila")
+arr.push([30,31,32,33,34,35,36,37,38,39])
+
+console.log(arr)  
+
+console.log(arr[2][arr[2].length-1], "el que voy a eliminar")
+
+arr[2].pop() // elimina el último
+console.log(arr[2])
+arr[2].splice(0,2) //elimino los dos repetidos
+console.log(arr[2])
+
+arr[2].reverse()
+console.log(arr[2])
+
+console.log(arr)
+
   
   // ¡Escribe tu código encima de esta línea!
